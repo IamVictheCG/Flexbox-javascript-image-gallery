73ef8b4c4252b0b7e898f60a710a5d2aa86dde54
Victor Okechukwu <VicOke360@gmail.com>
2023-03-22 10:51:47 +0100

    updates made

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let panels = document.querySelectorAll(".panels");
 
 function toggleActive() {
-    this.classList.toggle('open');
+    this.classList.toggle('.open');
+    this.classList.toggle('.open-active');
 }
 
-panels.forEach(panel => panel.addEventListener("mouseover", toggleActive))
+panels.forEach(panel => panel.addEventListener('click', toggleActive))
 // return activePanel
