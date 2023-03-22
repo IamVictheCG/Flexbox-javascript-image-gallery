dbc7da1a82ac8950390cbafc35dd8b2b8d195619
Victor Okechukwu <VicOke360@gmail.com>
2023-03-22 12:07:10 +0100

    updates made

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function removeClass(e) {
 
 panels.forEach(panel => {
     panel.addEventListener('click', toggleActive)
-    panel.addEventListener('click', toggleActive)
-    panel.addEventListener('transitionend', toggleActive)
+    panel.addEventListener('click', removeClass)
+    // panel.addEventListener('transitionend', toggleActive)
 })
 // return activePanel
