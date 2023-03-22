9415c584209068fee96a0f6916593726404ab8c2
Victor Okechukwu <VicOke360@gmail.com>
2023-03-22 12:06:34 +0100

    updates made

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function toggleActive(e) {
 
 function name(e) {
     panels.forEach(panel => {
-        if (e.propertyName.includes()) {
+        if (e.propertyName.includes('flex')) {
             panel.classList.remove('open')
         }
     })
