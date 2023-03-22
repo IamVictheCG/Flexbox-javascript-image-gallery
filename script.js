7089d9308703b17a457849598874620afc2e6451
Victor Okechukwu <VicOke360@gmail.com>
2023-03-22 10:58:58 +0100

    updates made

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-let panels = document.querySelectorAll(".panels");
+let panels = document.querySelectorAll(".panel");
+console.log(panels)
 
 function toggleActive() {
-    this.classList.toggle('.open');
+    // this.classList.toggle('open');
     this.classList.toggle('.open-active');
 }
 
